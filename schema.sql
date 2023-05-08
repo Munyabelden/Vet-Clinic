9703eea2e9912cf0e67a951958239eeea246a429
Gracerue <munyaradzi045@gmail.com>
2023-05-08 22:21:10 +0200

    Create table named animals

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
-/* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic
 
 CREATE TABLE animals (
-    name varchar(100)
+  id SERIAL PRIMARY KEY,
+  name VARCHAR (50), 
+  date_of_birth DATE NOT NULL,
+  escape_attemps INT,
+  neutered BOOLEAN NOT NULL,
+  weight_kg DECIMAL(10,2) NOT NULL
 );
