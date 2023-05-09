e501a479a1ec24e76d172258d0ad323474765ef9
Gracerue <munyaradzi045@gmail.com>
2023-05-09 13:22:00 +0200

    Update animls table

--- queries.sql
+++ queries.sql
@@ -13,3 +13,32 @@ SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name IN ('Gabumon');
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT ditti;
+UPDATE animals SET weight_kg = -1 * weight_kg;
+ROLLBACK TO ditti;
+UPDATE animals SET weight_kg = -1 * weight_kg WHERE weight_kg < 0;
+COMMIT;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attemps = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, SUM(escape_attemps) AS total_escapes FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM animals GROUP BY species;
+SELECT species, AVG(escape_attemps) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
