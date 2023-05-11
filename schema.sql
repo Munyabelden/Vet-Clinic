88199e7c0c36f8fd4b94f9cd85dde769f8e6fb29
Gracerue <munyaradzi045@gmail.com>
2023-05-11 13:55:01 +0200

    Joined tables

--- schema.sql
+++ schema.sql
@@ -23,10 +23,27 @@ CREATE TABLE specis (
   name VARCHAR(50),
 );
 
-ALTER TABLE vet_clinic
+ALTER TABLE animals
 DROP COLUMN species;
 
-ALTER TABLE vet_clinic
+ALTER TABLE animals
 ADD COLUMN species_id INTEGER REFERENCES species(id),
 ADD COLUMN owner_id INTEGER REFERENCES owners(id);
 
+CREATE TABLE vets (
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR(255), 
+  age INTEGER, 
+  date_of_graduation DATE 
+);
+
+CREATE TABLE specializations (
+  id SERIAL PRIMARY KEY,
+  vet_id INTEGER NOT NULL REFERENCES vets(id),
+  species_id INTEGER NOT NULL REFERENCES species(id)
+);
+
+CREATE TABLE visits ( 
+  vets_id INTEGER NOT NULL REFERENCES vets(id), 
+  animal_id INTEGER NOT NULL REFERENCES animals(id)
+);
